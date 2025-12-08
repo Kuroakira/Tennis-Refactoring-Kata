935ec6b1408e91292b48a508e936cb1916950dd5
Kuroakira <jinnseitanosi@gmail.com>
2025-12-08 16:09:56 +0900

    Refactor TennisGame3 to improve variable naming for player scores and names, streamline score calculation logic in getScore method, and enhance maintainability by utilizing a static array for score strings.

--- typescript/src/TennisGame3.ts
+++ typescript/src/TennisGame3.ts
@@ -1,34 +1,35 @@
 import { TennisGame } from './TennisGame';
 
 export class TennisGame3 implements TennisGame {
-  private p2: number = 0;
-  private p1: number = 0;
-  private p1N: string;
-  private p2N: string;
+  private player1Score: number = 0;
+  private player2Score: number = 0;
+  private player1Name: string;
+  private player2Name: string;
+  private static readonly scoreStrings: string[] = ['Love', 'Fifteen', 'Thirty', 'Forty'];
 
-  constructor(p1N: string, p2N: string) {
-    this.p1N = p1N;
-    this.p2N = p2N;
+  constructor(player1Name: string, player2Name: string) {
+    this.player1Name = player1Name;
+    this.player2Name = player2Name;
   }
 
   getScore(): string {
-    let s: string;
-    if (this.p1 < 4 && this.p2 < 4 && !(this.p1 + this.p2 === 6)) {
-      const p: string[] = ['Love', 'Fifteen', 'Thirty', 'Forty'];
-      s = p[this.p1];
-      return (this.p1 === this.p2) ? s + '-All' : s + '-' + p[this.p2];
-    } else {
-      if (this.p1 === this.p2)
-        return 'Deuce';
-      s = this.p1 > this.p2 ? this.p1N : this.p2N;
-      return (((this.p1 - this.p2) * (this.p1 - this.p2)) === 1) ? 'Advantage ' + s : 'Win for ' + s;
+    if (this.player1Score === this.player2Score && this.player1Score >= 3) {
+      return 'Deuce';
     }
+
+    if (this.player1Score < 4 && this.player2Score < 4) {
+      const result = TennisGame3.scoreStrings[this.player1Score];
+      return (this.player1Score === this.player2Score) ? result + '-All' : result + '-' + TennisGame3.scoreStrings[this.player2Score];
+    }
+
+    const leadingPlayer = this.player1Score > this.player2Score ? this.player1Name : this.player2Name;
+    return (Math.abs(this.player1Score - this.player2Score) === 1) ? `Advantage ${leadingPlayer}` : `Win for ${leadingPlayer}`;
   }
 
   wonPoint(playerName: string): void {
     if (playerName === 'player1')
-      this.p1 += 1;
+      this.player1Score += 1;
     else
-      this.p2 += 1;
+      this.player2Score += 1;
   }
 }
