0f77420ad5a3a998df1996853868f120e6cf6d28
Kuroakira <jinnseitanosi@gmail.com>
2025-12-15 08:20:16 +0900

    Refactor TennisGame5 to enhance score management by implementing a chain of responsibility pattern with dedicated score handler classes, improving code organization and maintainability. Introduced a Player class to encapsulate player state and behavior.

--- typescript/src/TennisGame5.ts
+++ typescript/src/TennisGame5.ts
@@ -1,68 +1,119 @@
 import { TennisGame } from './TennisGame';
 
 export class TennisGame5 implements TennisGame {
-  private player2Name : string;
-  private player1Name : string;
-  private player1Score: number;
-  private player2Score: number;
+  private player1: Player;
+  private player2: Player;
+  private readonly scores: string[] = ["Love", "Fifteen", "Thirty", "Forty"];
+  private scoreHandler: ScoreHandler;
 
   constructor(player1Name: string, player2Name: string) {
-    this.player1Name = player1Name;
-    this.player2Name = player2Name;
-    this.player1Score = 0;
-    this.player2Score = 0;
+    this.player1 = new Player(player1Name);
+    this.player2 = new Player(player2Name);
+
+    const winHandler = new WinHandler();
+    const advantageHandler = new AdvantageHandler();
+    const deuceHandler = new DeuceHandler();
+    const defaultHandler = new DefaultHandler();
+    winHandler.setNextHandler(advantageHandler);
+    advantageHandler.setNextHandler(deuceHandler);
+    deuceHandler.setNextHandler(defaultHandler);
+    this.scoreHandler = winHandler;
   }
 
   getScore(): string {
-    let p1 = this.player1Score;
-    let p2 = this.player2Score;
-
-    while (p1 > 4 || p2 > 4) {
-      p1--;
-      p2--;
+    const result = this.scoreHandler.handle(this.player1, this.player2);
+    if (result) {
+      return result;
     }
-
-    const lookup = {
-      0: {0: "Love-All",
-        1: "Love-Fifteen",
-        2: "Love-Thirty",
-        3: "Love-Forty",
-        4: "Win for player2"},
-
-      1: {0: "Fifteen-Love",
-        1: "Fifteen-All",
-        2: "Fifteen-Thirty",
-        3: "Fifteen-Forty",
-        4: "Win for player2"},
-
-      2: {0: "Thirty-Love",
-        1: "Thirty-Fifteen",
-        2: "Thirty-All",
-        3: "Thirty-Forty",
-        4: "Win for player2"},
-
-      3: {0: "Forty-Love",
-        1: "Forty-Fifteen",
-        2: "Forty-Thirty",
-        3: "Deuce",
-        4: "Advantage player2"},
-
-      4: {0: "Win for player1",
-        1: "Win for player1",
-        2: "Win for player1",
-        3: "Advantage player1",
-        4: "Deuce"}
-    }
-
-    // @ts-ignore
-    return lookup[p1][p2]
-
+    throw new Error("No handler found");
   }
 
   wonPoint(playerName: string): void {
-    if (playerName === 'player1')
-      this.player1Score += 1;
-    else
-      this.player2Score += 1;
+    if (playerName === this.player1.name)
+      this.player1.winPoint();
+    else if (playerName === this.player2.name)
+      this.player2.winPoint();
+  }
+}
+
+class Player {
+  readonly name: string;
+  score: number;
+  constructor(name: string) {
+    this.name = name;
+    this.score = 0;
+  }
+  getScore(): number {
+    return this.score;
+  }
+  winPoint() {
+    this.score += 1;
+  }
+}
+
+abstract class ScoreHandler {
+  nextHandler: ScoreHandler | null = null;
+
+  setNextHandler(handler: ScoreHandler) {
+    this.nextHandler = handler;
+    return handler;
+  }
+
+  handle(player1: Player, player2: Player): string | null {
+    const result = this.process(player1, player2);
+    if (result) {
+      return result;
+    }
+
+    if (this.nextHandler) {
+      return this.nextHandler.handle(player1, player2);
+    }
+
+    return null;
+  }
+
+  protected abstract process(player1: Player, player2: Player): string | null;
+}
+
+class DeuceHandler extends ScoreHandler{
+  protected process(player1: Player, player2: Player): string | null {
+    if ((player1.getScore() === player2.getScore()) && player1.getScore() >= 3) {
+      return "Deuce";
+    }
+    return null;
+  }
+}
+
+class AdvantageHandler extends ScoreHandler {
+  protected process(player1: Player, player2: Player): string | null {
+    console.log(player1.getScore(), player2.getScore());
+    const diffScore = Math.abs(player1.getScore() - player2.getScore());
+    if ((player1.getScore() >= 4 || player2.getScore() >= 4) && diffScore === 1) {
+      return `Advantage ${player1.getScore() > player2.getScore() ? player1.name : player2.name}`;
+    }
+    return null;
+  }
+}
+
+class WinHandler extends ScoreHandler {
+  protected process(player1: Player, player2: Player): string | null {
+    const diffScore = Math.abs(player1.getScore() - player2.getScore());
+    if (diffScore >= 2 && (player1.getScore() >= 4 || player2.getScore() >= 4)) {
+      return `Win for ${player1.getScore() > player2.getScore() ? player1.name : player2.name}`;
+    }
+    return null;
+  }
+}
+
+class DefaultHandler extends ScoreHandler {
+  private readonly scores: string[] = ["Love", "Fifteen", "Thirty", "Forty"];
+  protected process(player1: Player, player2: Player): string | null {
+    if (player1.getScore() < 4 && player2.getScore() < 4) {
+      if (player1.getScore() === player2.getScore()) {
+        return `${this.scores[player1.getScore()]}-All`;
+      }
+      return `${this.scores[player1.getScore()]}-${this.scores[player2.getScore()]}`;
+    }
+    return null;
   }
 }
